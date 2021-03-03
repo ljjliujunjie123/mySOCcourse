a744b077bfda82391ad5884a6567bb6dcf8fa2ab
ljjliujunjie123 <ljjliujunjie123@163.com>
2021-03-03 13:05:22 +0800

    my hw1 source code first commit

---
+++ hw1_1/Debug/main.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/some_useful_program/ccs/ccsv5/ccs_base/msp430/include/msp430.h
+main.obj: D:/some_useful_program/ccs/ccsv5/ccs_base/msp430/include/msp430g2553.h
+main.obj: D:/some_useful_program/ccs/ccsv5/ccs_base/msp430/include/in430.h
+
+../main.c: 
+D:/some_useful_program/ccs/ccsv5/ccs_base/msp430/include/msp430.h: 
+D:/some_useful_program/ccs/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+D:/some_useful_program/ccs/ccsv5/ccs_base/msp430/include/in430.h: 
